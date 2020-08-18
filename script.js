61ea2f547e790125aceeb5b9c45581f2fb0ec834
jthinlay <jthinlay@hotmail.com>
2020-08-18 12:21:34 -0700

    Intial commit

---
+++ script.js
@@ -0,0 +1,89 @@
+const imageContainer =  document.getElementById('image-container');
+const loader = document.getElementById("loader");
+
+let ready = false; 
+let imagesLoaded = 0;
+let totalImages = 0; 
+let initialLoad = true;
+
+let photosArray = [];
+
+// helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes){
+    for(const key in attributes){
+        element.setAttribute(key, attributes[key]);
+    }
+}
+//Check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++; 
+    if(imagesLoaded === totalImages){
+        ready = true; 
+        loader.hidden = true;
+        initialLoad = false;
+        count = 30; 
+    } 
+}
+
+// Create Elements for links and Photos, Add to DOM
+function displayPhotos(){
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    console.log('total images:', totalImages)
+    loader.hidden = true;
+    photosArray.forEach((photo)=>{
+        const item = document.createElement('a');
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank');
+
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        })
+
+        const img = document.createElement('img');
+        // img.setAttribute('src', photo.urls.regular);
+        // img.setAttribute('alt', photo.alt_description);
+        // img.setAttribute('title', photo.alt_description);
+
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
+        // Event Listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded);
+
+        //Put <img> inside <a>, then put both inside imageContainer Element 
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    })
+}
+
+// Unsplash API URL 
+let count = 5;
+const apiKey = '3eL2yadZ1W5uXQViyC_N9T3KagXIfS2iqKD9poRuj-Q';
+const unsplashApiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+async function getUnsplashApiPhotos(){
+    try{
+        const response = await fetch(unsplashApiUrl);
+        photosArray = await response.json();
+
+        displayPhotos();
+
+    }catch(error){
+        console.log("Whoops! something went wrong", error)
+    }
+}
+
+// Check to see if scrolling near bottom of page, load more photos
+window.addEventListener('scroll', () => {
+    if ( window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+        ready = false;
+        getUnsplashApiPhotos();
+    }
+})
+getUnsplashApiPhotos();
+
+
